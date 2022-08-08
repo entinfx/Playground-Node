7131ddf7ccb14ddda0b3cd6a3f678d4c3d4976d1
entinfx <monkacry@gmail.com>
2022-08-08 08:49:51 +0400

    Use EJS for views, set up basic views.

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express')
 const app = express()
 
+// Use EJS as a View Engine
+app.set('view engine', 'ejs')
+// EJS automatically looks for views in the ./views folder. To use another
+// folder use the following
+// app.set('views', 'my-views')
+
+// Listen for requests on localhost:3000
 app.listen(3000)
 
 // Once a request is sent to the browser, the rest of the file isn't executed
 // until the next request is performed.
 app.get('/', (req, res) => {
     // sendFile automatically sets header and status code
-    res.sendFile('./views/index.html', { root: __dirname })
+    // res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    // res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about')
 })
 
+// Redirects
 app.get('/about-us', (req, res) => {
     // redirect sets status code automatically
     res.redirect('/about')
 })
 
-// If no requests above matched, this middleware function fires
+// 404: Not found - if no requests above matched, this middleware function fires
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    // res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404')
 })
