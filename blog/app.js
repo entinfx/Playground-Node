ce7284b66c6d14bb9077701e0d22a0e78ddbb270
entinfx <monkacry@gmail.com>
2022-08-14 14:18:56 +0400

    Add MongoDB (not implemented on server)

--- blog/app.js
+++ blog/app.js
@@ -1,19 +1,29 @@
-// Set up Express
+// Imports
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+// Express app
 const app = express()
 
-// Middleware imports
-const morgan = require('morgan')
+// Set up database (not implemented)
+const mongoURI = 'URI to MongoDB'
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        console.log(`Connected to MongoDB: ${result}`)
 
-// View engine
+        app.listen(3000, () => {
+            console.log('Listening on localhost:3000...')
+        })
+    })
+    .catch((error) => {
+        console.log(`Couldn't connect to MongoDB: ${error}`)
+    })
+
+// View engine for Express
 app.set('view engine', 'ejs')
 
-// Run server at localhost:3000
-app.listen(3000, () => {
-    console.log('Listening on localhost:3000...')
-})
-
-// Middleware
+// Middleware (runs on every request)
 app.use(morgan('dev'))
 app.use(express.static('public'))
 
