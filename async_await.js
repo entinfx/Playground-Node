e033ac0c0740a59d6c5ab2c59a7e91fe3aafad3b
entinfx <monkacry@gmail.com>
2022-10-04 01:22:13 +0500

    _

--- async_await.js
+++ async_await.js
@@ -3,45 +3,62 @@ function getData() {
         const errorOccurred = false
 
         if (errorOccurred) {
-            reject('getData(): Error!')
-        } else {
-            resolve('Some cool data')
+            reject('Error: Could not get data.')
+            return
         }
+
+        resolve('Some cool data.')
     })
 }
 
-// We can also use the 'async' keyword to make the function return a resolved
-// promise of a value it's returning
-async function processData(data) {
-    data += ', and more data!'
-    return data
+function processData(data) {
+    return new Promise((resolve, reject) => {
+        const errorOccurred = false
+
+        if (errorOccurred) {
+            reject('Error: Could not process data.')
+            return
+        }
+
+        resolve(data + ' And more data!')
+    })
 }
 
-// Without async await
-// getData()
-//     .then(data => {
-//         console.log(data)
-//         return processData(data)
-//     })
-//     .then(processedData => {
-//         console.log(processedData)
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     })
+// Run getData() and processData() using then/catch:
+getData().then(data => {
+    console.log('Received: ' + data)
+    return processData(data)
+}).then(processedData => {
+    console.log('Processed: ' + processedData)
+}).catch(error => {
+    console.log(error)
+})
 
-// With async await
-// * 'async' makes the function's return value a promise resolved to that value
-// * 'async' also sets up a context to use 'await'
+// Run getData() and processData() using async/await:
+// * 'async' sets up the context for 'await'
 // * 'await' will wait for the resolved value of a promise and return that value
 // * All promise rejects will be caught in the catch block
-async function testAsyncAwait() {
+async function getProcessedData() {
     try {
-        // Multiple awaits in a row will make them wait for each other:
-        // const data = await getData()
-        // const moreData = await getData()
+        const data = await getData()
+        console.log('Received: ' + data)
+        const processedData = await processData(data)
+        console.log('Processed: ' + processedData)
+    } catch(error) {
+        console.log(error)
+    }
+}
+getProcessedData()
 
-        // to run them concurrently, group them into a single promise and await
+// * 'async' functions explicitly return a resolved promise:
+async function resolvedPromise() {
+    return 'Promise resolved to this String'
+}
+console.log(resolvedPromise())
+
+// Use 'Promise.all' to await multiple promises concurrently:
+async function getMoreData() {
+    try {
         const data = getData()
         const moreData = getData()
         const allData = await Promise.all([data, moreData])
@@ -52,4 +69,4 @@ async function testAsyncAwait() {
     }
 }
 
-testAsyncAwait()
+console.log(getMoreData())
